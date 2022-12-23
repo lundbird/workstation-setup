487dad92e0c13a9b94defefa89e546dc4bec022b
Alex Lundberg <alexlundberg@Alexs-MacBook-Air.local>
2022-12-22 21:39:20 -0600

    add another file

---
+++ install_zsh.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+sh -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
+git clone https://github.com/zsh-users/zsh-autosuggestions ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-autosuggestions
+git clone https://github.com/zsh-users/zsh-completions ${ZSH_CUSTOM:-${ZSH:-~/.oh-my-zsh}/custom}/plugins/zsh-completions
+git clone https://github.com/zsh-users/zsh-syntax-highlighting.git
+cp zsh/alexlundberg.zsh-theme ~/.oh-my-zsh/custom/themes/alexlundberg.zsh-theme
